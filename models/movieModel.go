968ed5a25a68d9aab2ed78bfe7736d13f1ce3319
AmmarFarid10 <91678027+AmmarFarid10@users.noreply.github.com>
2023-02-24 11:36:03 +0500

    Update movieModel.go

--- models/movieModel.go
+++ models/movieModel.go
@@ -10,7 +10,7 @@ type Movie struct {
 	Id         primitive.ObjectID `bson:"_id"`
 	Name       *string            `json:"name" validate:"required"`
 	Topic      *string            `json:"topic" validate:"required"`
-	Genre_id   *string            `json:"genre_id" validate:"required"`
+	Genre_id   int            `json:"genre_id" validate:"required"`
 	Movie_URL  *string            `json:"movie_url" validate:"required"`
 	Created_at time.Time          `json:"created_at"`
 	Updated_at time.Time          `json:"updated_at"`
